f52c91734539223a9d10c3d4da7bc87792e5eae1
Sam Allison <samuel.allison.15@cnu.edu>
2017-03-05 12:35:32 -0500

    made table visible

--- src/SongLibrary.java
+++ src/SongLibrary.java
@@ -1,13 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.LayoutManager;
+//import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.Box;
-import javax.swing.BoxLayout;
+//import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,7 +15,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+//import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 
@@ -23,7 +24,7 @@ import javax.swing.SwingUtilities;
 public class SongLibrary extends JFrame{
 	
 	private final String [] HEADERS = {"Song", "Artist", "Album", "Year"};
-    private Object [][] DATA;
+    private Object [][] DATA = {};
     
 	public SongLibrary(){
         super("SongLibrary");
@@ -47,6 +48,8 @@ public class SongLibrary extends JFrame{
         JMenuItem about = Library.add("About...");
         Library.addSeparator();
         JMenuItem exit = Library.add("Exit");
+        
+        
         
         JMenu tables = new JMenu("Table");
         menubar.add(tables);
@@ -77,15 +80,22 @@ public class SongLibrary extends JFrame{
                askForClosing();
            }
         });
+        
+        
+        
         JTable table = new JTable( DATA, HEADERS );
         table.setPreferredScrollableViewportSize(new Dimension(500, 100));
         table.setFillsViewportHeight(true);
-        //pack();
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(BorderLayout.CENTER, scrollPane);
+        
+        
+        pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
     protected void askForClosing() {
-        int result = JOptionPane.showConfirmDialog(this, "Do you want to exit");
+        int result = JOptionPane.showConfirmDialog(this, "Do you want to exit?");
         if(result == JOptionPane.YES_OPTION){
             dispose();
         }
